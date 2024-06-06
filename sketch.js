cc785285bd2ca63aa62d8fde6669f76ccbc59ba7
Mirian <miriamsalesdasilva123@gmail.com>
2024-06-06 12:20:41 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
+  
+  
+  function draw() { 
+    stroke("blue"); 
+    fill("red");
+  
+    if(mouseIsPressed){
+       rect(mouseX, mouseY, 20, 35);
+  
+    }
+        
+   }
